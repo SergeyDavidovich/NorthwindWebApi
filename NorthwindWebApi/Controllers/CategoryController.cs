e0a6948fd9f07ad69499adeecf5f9a47ddbecc5c
Sergiusz Kriwobokow <writesd@hotmail.com>
2024-05-13 13:05:35 +0200

    added CategoryController

---
+++ NorthwindWebApi/Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NorthwindWebApi.Models;
+using System.Collections.ObjectModel;
+
+namespace NorthwindWebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet("Categories")]
+        public IEnumerable<Category> Get()
+        {
+            return new List<Category>();
+        }
+    }
+}
